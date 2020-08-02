fd7018a0ec728a91c82a84c45dc8bb1129fac23c
尚万中 <shangwanzhong@huohua.cn>
2020-08-02 17:04:49 +0800

    pm2

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 ### 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:shwzh/shwzh.github.io.git master
+git push -f git@github.com:shwzh/shwzh.github.io.git master:gh-pages
 
 ### 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 ### git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
