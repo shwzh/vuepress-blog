ecd761449ba8439eb113b3f5a1f18fbfe0dad463
尚万中 <shangwanzhong@huohua.cn>
2020-08-02 11:58:21 +0800

    my first push vuepess blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: '飞鸿的博客',
+    description: '我的心路历程',
+    dest: './dist',
+    port: '5555',
+    head: [
+        ['link', {rel: 'icon', href: '/logo.gif'}]
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        nav: require('./nav'),
+        sidebar: require('./sidebar'),
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+        searchMaxSuggestoins: 10,
+        serviceWorker: {
+            updatePopup: {
+                message: "New content is available.",
+                buttonText: 'Refresh'
+            }
+        },
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页 ！'
+    }
+}
+
